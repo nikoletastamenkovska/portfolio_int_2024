14154547a83fb59709c00f20d5dfc9044d619100
Nikoleta Stamenkovska-Stojmirovska <nikoleta.stamenkovska@yahoo.com>
2024-09-01 16:58:09 +0200

    Finished the animation for about section.

--- scripts/word_animation.js
+++ scripts/word_animation.js
@@ -6,3 +6,27 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+// the animating about_wrapper when is needed to start after loading the section;
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('section');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('active');
+                const spans = entry.target.querySelectorAll('.about_wrapper p span');
+                spans.forEach((span, idx) => {
+                    span.style.setProperty('--animation-delay', `${idx * .4}s`);
+                });
+            } else {
+                entry.target.classList.remove('active');
+            }
+        });
+    }, {
+        threshold: 0.5
+    });
+
+    sections.forEach(section => {
+        observer.observe(section)
+    });
+});
