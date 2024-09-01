26558d3f217ce51be0285030a53df6da97a3fc6a
Nikoleta Stamenkovska-Stojmirovska <nikoleta.stamenkovska@yahoo.com>
2024-09-01 19:18:04 +0200

    Sckills copleted

--- scripts/random_position.js
+++ scripts/random_position.js
@@ -10,4 +10,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
         span.style.opacity = Math.random() > .5 ? 1 : 0
     });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const list = document.querySelectorAll('.wrapper_reverse span');
+
+    list.forEach(span => {
+        const duration = Math.random() * 10 + 20;
+        const delay = Math.random() * 10;
+
+        span.style.animationDuration = `${duration}s`;
+        span.style.animationDelay = `-${delay}s`;
+
+        span.style.opacity = Math.random() > .5 ? 1 : 0
+    });
 })
--- scripts/word_animation.js
+++ scripts/word_animation.js
@@ -30,3 +30,40 @@ document.addEventListener('DOMContentLoaded', () => {
         observer.observe(section)
     });
 });
+
+// The animation for skills wrapper
+document.addEventListener("DOMContentLoaded", function () {
+    const section = document.querySelector(".skills");
+    const listItems = section.querySelectorAll(".animate_list_item");
+
+    listItems.forEach(item => {
+        const text = item.textContent;
+        const words = text.split(/\s+/);
+        item.innerHTML = words.map(word => `<span class="word_animate">${word}</span>`).join(' ');
+        item.setAttribute('data-content', text);
+    });
+
+    const animatedWords = section.querySelectorAll(".word_animate");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('active');
+                animateWords();
+            }
+        });
+    }, {
+        threshold: 0.5
+    });
+
+    observer.observe(section);
+
+    function animateWords() {
+        animatedWords.forEach((word, idx) => {
+            setTimeout(() => {
+                word.classList.add("fade_in_slide");
+            }, idx * 150);
+        });
+    }
+});
+
