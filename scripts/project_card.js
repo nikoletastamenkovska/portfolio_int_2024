6900ef2543794d2455e065e93f10360be536ee0f
Nikoleta Stamenkovska-Stojmirovska <nikoleta.stamenkovska@yahoo.com>
2024-09-04 14:57:43 +0200

    Project card modifications

--- scripts/project_card.js
+++ scripts/project_card.js
@@ -4,14 +4,23 @@ window.createProjectCard = function (projects) {
         const card = document.createElement('li');
         card.classList.add('project_card');
 
-        card.innerHTML = `
+        const cover = document.createElement('div');
+        cover.classList.add('cover');
+        cover.innerHTML = `
+            <img src="${project.images[0]}" alt="{project.title}"></img>
+        `
+
+        const cardInner = document.createElement('div');
+        cardInner.classList.add('card_inner');
+
+        cardInner.innerHTML = `
             <h3>${project.title}</h3>
-            
             <p>Technologies: ${project.tech.join(', ')}</p>
             <a href="${project.link}" target="_blank">GitHub</a>
             ${project.deployed ? `<a href="${project.deployed}" target="_blank">Live Demo</a>` : ''}
         `;
 
+        card.append(cover, cardInner)
         container.appendChild(card);
     });
 }
