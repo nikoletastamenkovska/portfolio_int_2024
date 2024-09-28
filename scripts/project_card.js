8d09e3db23633fd6158e3eb2836d699eb28b760d
Nikoleta Stamenkovska-Stojmirovska <nikoleta.stamenkovska@yahoo.com>
2024-09-28 07:41:34 +0200

    Finished the acroller functionallity on the project cards.

--- scripts/project_card.js
+++ scripts/project_card.js
@@ -2,22 +2,22 @@ window.createProjectData = function (projects) {
     const container = document.getElementById('projects_list_container');
     const scroller = document.querySelector('.scroller_inner');
 
-    projects.forEach(project => {
-        // the card
-        const card = document.createElement('li');
-        card.classList.add('project_card');
+    function createCards(projects) {
+        projects.forEach(project => {
+            const card = document.createElement('li');
+            card.classList.add('project_card');
 
-        const cover = document.createElement('div');
-        cover.classList.add('cover');
-        cover.style.backgroundImage = `url(${project.images[0]})`;
+            const cover = document.createElement('div');
+            cover.classList.add('cover');
+            cover.style.backgroundImage = `url(${project.images[0]})`;
 
-        const cardInner = document.createElement('div');
-        cardInner.classList.add('card_inner');
-        const projectImagesBtn = document.createElement('button');
-        projectImagesBtn.classList.add('project_images_button');
-        projectImagesBtn.textContent = 'Project Images';
+            const cardInner = document.createElement('div');
+            cardInner.classList.add('card_inner');
+            const projectImagesBtn = document.createElement('button');
+            projectImagesBtn.classList.add('project_images_button');
+            projectImagesBtn.textContent = 'Project Images';
 
-        cardInner.innerHTML = `
+            cardInner.innerHTML = `
             <div class="inner_card_content">
                 <h3 class="card_title">${project.title}</h3>
                 <div class="card_description">${project.description}</div>
@@ -30,89 +30,94 @@ window.createProjectData = function (projects) {
             </div>
         `;
 
-        const carousel = document.createElement('div');
-        carousel.id = 'carousel';
-        carousel.style.display = 'none';
+            const carousel = document.createElement('div');
+            carousel.id = 'carousel';
+            carousel.style.display = 'none';
 
-        const carouselNavigation = document.createElement('div');
-        carouselNavigation.id = 'carousel_nav';
+            const carouselNavigation = document.createElement('div');
+            carouselNavigation.id = 'carousel_nav';
 
 
-        const imagesList = [];
-        const dots = [];
+            const imagesList = [];
+            const dots = [];
 
-        project.images.forEach((image, index) => {
-            const img = document.createElement('img');
-            img.src = image;
-            img.alt = `Project image ${index + 1}`;
-            img.classList.add('carousel_image');
-            imagesList.push(img);
+            project.images.forEach((image, index) => {
+                const img = document.createElement('img');
+                img.src = image;
+                img.alt = `Project image ${index + 1}`;
+                img.classList.add('carousel_image');
+                imagesList.push(img);
 
-            // create dot element
-            const dot = document.createElement('span');
-            dot.classList.add('dot');
-            dots.push(dot);
+                const dot = document.createElement('span');
+                dot.classList.add('dot');
+                dots.push(dot);
 
-            carousel.appendChild(img);
-            carouselNavigation.appendChild(dot);
+                carousel.appendChild(img);
+                carouselNavigation.appendChild(dot);
 
-            dot.addEventListener('click', () => {
+                dot.addEventListener('click', () => {
+                    imagesList.forEach(img => img.classList.remove('active'));
+                    dots.forEach(dot => dot.classList.remove('active'));
+
+                    img.classList.add('active');
+                    dot.classList.add('active');
+                });
+            });
+
+            if (imagesList.length > 0) {
+                imagesList[0].classList.add('active');
+                dots[0].classList.add('active');
+            }
+
+            carousel.appendChild(carouselNavigation);
+            cardInner.append(projectImagesBtn);
+            card.append(cover, cardInner, carousel);
+            container.appendChild(card);
+
+
+            card.addEventListener('mouseenter', () => {
+                const cardRect = card.getBoundingClientRect();
+                const scrollRect = scroller.getBoundingClientRect();
+
+                if (cardRect.right > scrollRect.right || cardRect.left < scrollRect.left) {
+                    scroller.scrollBy({
+                        left: cardRect.left - scrollRect.left,
+                        behavior: 'smooth'
+                    });
+                }
+            });
+
+            card.addEventListener('mouseleave', () => {
+                carousel.style.display = "none";
+                Array.from(cardInner.children).forEach(child => {
+                    if (child != carousel) {
+                        if (child === projectImagesBtn) {
+                            child.style.display = 'block';
+                        } else {
+                            child.style.display = 'flex';
+                        }
+                    }
+                })
+            });
+
+            projectImagesBtn.addEventListener('click', () => {
+                Array.from(cardInner.children).forEach(child => {
+                    if (child != carousel) {
+                        child.style.display = "none";
+                    }
+                });
+                carousel.style.display = "flex";
+
                 imagesList.forEach(img => img.classList.remove('active'));
                 dots.forEach(dot => dot.classList.remove('active'));
-
-                img.classList.add('active');
-                dot.classList.add('active');
+                imagesList[0].classList.add('active');
+                dots[0].classList.add('active');
             });
         });
+    }
 
-        if (imagesList.length > 0) {
-            imagesList[0].classList.add('active');
-            dots[0].classList.add('active');
-        }
-
-        carousel.appendChild(carouselNavigation);
-        cardInner.append(projectImagesBtn);
-        card.append(cover, cardInner, carousel);
-        container.appendChild(card);
-
-
-        card.addEventListener('mouseenter', () => {
-            const cardRect = card.getBoundingClientRect();
-            const scrollRect = scroller.getBoundingClientRect();
-
-            if (cardRect.right > scrollRect.right || cardRect.left < scrollRect.left) {
-                scroller.scrollBy({
-                    left: cardRect.left - scrollRect.left,
-                    behavior: 'smooth'
-                });
-            }
-        });
-
-        card.addEventListener('mouseleave', () => {
-            carousel.style.display = "none";
-            Array.from(cardInner.children).forEach(child => {
-                if (child != carousel) {
-                    if (child === projectImagesBtn) {
-                        child.style.display = 'block';
-                    } else {
-                        child.style.display = 'flex';
-                    }
-                }
-            })
-        });
-
-        projectImagesBtn.addEventListener('click', () => {
-            Array.from(cardInner.children).forEach(child => {
-                if (child != carousel) {
-                    child.style.display = "none";
-                }
-            });
-            carousel.style.display = "flex";
-
-            imagesList.forEach(img => img.classList.remove('active'));
-            dots.forEach(dot => dot.classList.remove('active'));
-            imagesList[0].classList.add('active');
-            dots[0].classList.add('active');
-        });
-    });
+    createCards(projects);
+    createCards(projects);
+    createCards(projects);
+    createCards(projects);
 };
--- scripts/scroller.js
+++ scripts/scroller.js
@@ -1,23 +1,45 @@
-const scrollers = document.querySelectorAll('.scroller');
+function addScrollerAnimation() {
+    const scrollers = document.querySelectorAll('.scroller');
 
-if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-    addAnimation();
+    if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+        addAnimation();
+    }
+
+    function addAnimation() {
+        scrollers.forEach(scroller => {
+            scroller.setAttribute("data-animated", true);
+
+            const scrollerInner = document.querySelector('.scroller_inner');
+            let scrollTimeout;
+
+            scroller.addEventListener('scroll', () => {
+                scrollerInner.style.animationPlayState = 'paused';
+                clearTimeout(scrollTimeout);
+
+                if (scroller.scrollLeft >= scroller.scrollWidth / 3) {
+                    scroller.scrollLeft = 0;
+                }
+
+                scrollTimeout = setTimeout(() => {
+                    scrollerInner.style.animationPlayState = 'running';
+                }, 3000);
+            });
+        });
+    }
 }
 
-function addAnimation() {
-    scrollers.forEach(scroller => {
-        scroller.setAttribute("data-animated", true);
-
-        const scrollerInner = document.querySelector('.scroller_inner');
-        let scrollTimeout;
-
-        scroller.addEventListener('scroll', () => {
-            scrollerInner.style.animationPlayState = 'paused';
-            clearTimeout(scrollTimeout);
-
-            scrollTimeout = setTimeout(() => {
-                scrollerInner.style.animationPlayState = 'running';
-            }, 3000)
+function observeSectionActivation() {
+    const section = document.querySelector('#projects');
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                addScrollerAnimation();
+            }
         });
     });
+    observer.observe(section);
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    observeSectionActivation();
+});
